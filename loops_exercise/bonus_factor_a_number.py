7fff2015a64a1920293fcefa3f7f123b3b63c8d8
Ziqing Wang <redtaq@hotmail.com>
2018-03-06 19:50:53 +0000

    some modify

--- loops_exercise/bonus_factor_a_number.py
+++ loops_exercise/bonus_factor_a_number.py
@@ -1,5 +1,6 @@
 number = int(input("Give a number to start: "))
 factors = [n for n in range(1, int(round(number/2)) + 1) if number % n == 0]
+factors.append(number)
 for num in set(factors):
     print(num)
     
